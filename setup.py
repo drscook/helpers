1a9a941829977cac8bb5a9a09709872320b10560
Scott Cook <scook@tarleton.edu>
2018-07-27 14:43:22 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -59,22 +59,22 @@ def test_numba_cuda():
         except cuda.CudaSupportError:
             print("Are you sure you have a GPU?  If using Colab, Runtime->Change Runtime Type->Hardware accelerator = GPU")
             raise SystemExit()
-        except nb.OSError:
-            print('caught nb.OSError')
-            return False
-        except cuda.OSError:
-            print('caught cuda.OSError')
-            return False
-        except OSError:
-            print('caught OSError')
-            return False
-        except NvvmSupportError:
-            print('caught NvvmSupportError')
-            return False
-        except nb.NvvmSupportError:
-            print('caught nb.NvvmSupportError')
-            return False
-        except cuda.NvvmSupportError:
+#         except nb.OSError:
+#             print('caught nb.OSError')
+#             return False
+#         except cuda.OSError:
+#             print('caught cuda.OSError')
+#             return False
+#         except OSError:
+#             print('caught OSError')
+#             return False
+#         except NvvmSupportError:
+#             print('caught NvvmSupportError')
+#             return False
+#         except nb.NvvmSupportError:
+#             print('caught nb.NvvmSupportError')
+#             return False
+        except cudadrv.NvvmSupportError:
             print('caught cuda.NvvmSupportError')
             return False
         
