07eb2b42c0a57d90323c94eab66597106b00f0b9
Scott Cook <scook@tarleton.edu>
2018-09-08 06:06:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ import numba as nb
    
 def setup_numba_cuda():
     def test_numba_cuda():
-        print("\n")
         import numba as nb
         import numba.cuda as cuda
         A = np.arange(3)
@@ -59,45 +58,47 @@ def setup_numba_cuda():
             return False, "Are you sure you have a GPU?  If using Colab, Runtime->Change Runtime Type->Hardware accelerator = GPU"  # Add message to stop early
         except cuda.cudadrv.nvvm.NvvmSupportError:
             return False, None
+
         A *= 2
         if np.allclose(A, A_gpu.copy_to_host()):
-            return True, "\nCuda.jit IS INSTALLED AND WORKINNG!!  IGNORE ANY MESSAGES ABOVE THAT SAY DIFFERENTLY!!\n"
+            return True, "Numba Cuda IS INSTALLED AND WORKINNG!!  IGNORE ANY MESSAGES ABOVE THAT SAY DIFFERENTLY!!\n"
         else:
             return False, None
 
+    def install_none():
+        pass
+        
     def install_conda():
-        os.system('conda update conda')
-        os.system('conda install -c numba numba')
+        os.system('conda update conda')        
         os.system('conda install -c numba cudatoolkit')
+        os.system('conda install -c numba numba')
 
     def install_pip():
         os.system('apt-get update')
-        os.system('pip install --upgrade numba')
         os.system('apt install -y --no-install-recommends -q nvidia-cuda-toolkit')
         os.system('apt-get update')
         os.environ['NUMBAPRO_LIBDEVICE'] = "/usr/lib/nvidia-cuda-toolkit/libdevice"
         os.environ['NUMBAPRO_NVVM'] = "/usr/lib/x86_64-linux-gnu/libnvvm.so"
+        os.system('pip install --upgrade numba')
 
     # Loop over installation options
-    is_working, message = test_numba_cuda()
-    installs = [install_conda, install_pip]
-    while not is_working:
-        if message or not installs:
-            break
-            
-        install_func = installs.pop()
-        print(f"Cuda.jit not working yet.  Trying to {install_func.__name__.replace('_', ' ')}")
+    install_funcs = [install_none, install_conda, install_pip]
+    for install_func in install_funcs:
+        print('-----------------------------------------------------------------------------------------------------')
+        print(f"Trying to setup numba_cuda via {install_func.__name__.replace('_', ' ')}")
         try:
             install_func()
         except:
             pass
         else:
             is_working, message = test_numba_cuda()
-
-    if message:
-        print(message)
-    else:  
-        print("That failed too.  I give up.  Are you sure you have a GPU?  If using Colab, Runtime->Change Runtime Type->Hardware accelerator = GPU")
+            if message:
+                print(message)
+            if is_working:
+                print("THAT WORKED!!")
+                break
+            else:
+                print("That failed.")
 
     return is_working
               
