ae5d0bd9c69f089f6e158aafed5ddbbe81a5add5
Scott Cook <scook@tarleton.edu>
2022-12-14 09:45:30 -0600

    Update helpers.py

--- config/helpers.py
+++ config/helpers.py
@@ -1,1 +1,13 @@
-import numpy as np
+def listify(X):
+    """Turns almost anything into a list"""
+    if isinstance(X, list):
+        return X
+    elif (X is None) or (X is np.nan) or (X==''):
+        return []
+    elif isinstance(X, str):
+        return [X]
+    else:
+        try:
+            return list(X)
+        except:
+            return [X]
