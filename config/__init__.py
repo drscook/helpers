032921d26d1617d727a5a84e8109d7f428fb8ccd
Scott Cook <scook@tarleton.edu>
2022-12-14 11:07:25 -0600

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,3 @@
 import os, sys, pathlib, warnings, time, json, dataclasses
-import numpy as np, pandas as pd, matplotlib.pyplot as plt
+#import numpy as np, pandas as pd, matplotlib.pyplot as plt
 print('hi')
