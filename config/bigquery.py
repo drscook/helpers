39d3f81d49a8bd45dc8e9b1a15051602f53b7c04
Scott Cook <scook@tarleton.edu>
2022-12-14 13:54:47 -0600

    Update bigquery.py

--- config/bigquery.py
+++ config/bigquery.py
@@ -1,11 +1,10 @@
 import utils
 from google.colab import auth
 from google.cloud.bigquery import Client
-auth.authenticate_user()
-
 
 class BQ():
   def __init__(self, project_id):
+    auth.authenticate_user()
     self.client = Client(project=project_id)
   
   def run_query(self, qry):
