b31a6446fd5d645093aea7728602d1f04b9b6339
Scott Cook <scook@tarleton.edu>
2022-12-14 12:54:26 -0600

    Update utils.py

--- config/utils.py
+++ config/utils.py
@@ -11,3 +11,28 @@ def listify(X):
             return list(X)
         except:
             return [X]
+
+def to_numeric(ser):
+    """converts columns to small numeric dtypes when possible"""
+    dt = str(ser.dtype)
+    if  dt in ['geometry', 'timestamp'] or dt[0].isupper():
+        return ser
+    else:
+        return pd.to_numeric(ser, errors='ignore', downcast='integer')  # cast to numeric datatypes where possible
+
+def prep(df, fix_names=True):
+    """prep dataframes"""
+    idx = len(df.index.names)
+    df = df.reset_index()
+    if fix_names:
+        df.columns = [c.strip().lower() for c in df.columns]
+    return df.apply(to_numeric).convert_dtypes().set_index(df.columns[:idx].tolist())
+
+def run_query(qry):
+    res = client.query(qry).result()
+    if res.total_rows > 0:
+        res = prep(res.to_dataframe())
+        if 'geometry' in res.columns:
+            geo = gpd.GeoSeries.from_wkt(res['geometry'], crs=CRS['bigquery'])
+            res = gpd.GeoDataFrame(res, geometry=geo)
+    return res
