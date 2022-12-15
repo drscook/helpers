b22f09dd397b64aeb665e10f2865186aea25490d
Scott Cook <scook@tarleton.edu>
2022-12-14 20:16:43 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,19 @@
-import os, sys, pathlib, warnings, time, json, dataclasses
-import numpy as np, pandas as pd, geopandas as gpd, matplotlib.pyplot as plt
+pkgs = (
+    'os', 'sys', 'pathlib', 'warnings', 'time', 'json', 'dataclasses',
+    ('numpy', 'np'), ('pandas', 'pd'), ('geopandas', 'gpd'), ('matplotlib.pyplot', 'plt'),
+)
+for p in pkgs:
+    if isinstance(p, str):
+        name, alias = p, p
+    elif len(p) > 1:
+        name, alias = p[0], p[1]
+    else:
+        name, alias = p[0], p[0]
+    cmd = f'import {name} as {alias}'
+    try:
+        exec(cmd)
+    except ModuleNotFoundError:
+        pip = f'pip install {name}'
+        print(pip)
+        os.system(pip)
+        exec(cmd)
