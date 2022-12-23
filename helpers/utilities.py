5aec8208383c0476435aa295329d652be9ef05c1
Scott Cook <scook@tarleton.edu>
2022-12-23 08:53:12 -0600

    Update utilities.py

--- helpers/utilities.py
+++ helpers/utilities.py
@@ -81,7 +81,7 @@ def ljust(msg, width, fillchar='0'):
     return str(msg).ljust(width, fillchar)
 
 def replace(msg, table):
-    for pats, repl in table.items():
+    for pats, repl in table:
         for pat in listify(pats):
             msg = msg.replace(pat, repl)
     return msg
