5b545d0f11d060aab88b2f04bb51bf78ff055c0b
drscook <scook@tarleton.edu>
2022-12-27 18:40:56 -0600

    patch

--- helpers/common_imports.py
+++ helpers/common_imports.py
@@ -1,6 +1,8 @@
-import os, sys, pathlib, shutils, warnings, time, datetime, json, dataclasses, codetiming
+import os, sys, pathlib, shutils, warnings, time, datetime, json, dataclasses
 import itertools as it, numpy as np, pandas as pd, matplotlib.pyplot as plt
+from codetiming import Timer as Timer
 
+ellipsis = ' ... '
 CRS = {
     'census'  : 'EPSG:4269'  , # degrees - used by Census
     'bigquery': 'EPSG:4326'  , # WSG84 - used by Bigquery
